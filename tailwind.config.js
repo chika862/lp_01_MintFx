b63441804ec9a0336c2b5e88716782bdf57cde0b
pin863 <chika67zz@gmail.com>
2025-10-29 12:08:27 +0900

    swiperでテスト

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1A1A1A",
+        accent: "#1D8382",
+        background: "#F5F5F7",
+      },
+    },
   },
   plugins: [],
 };
