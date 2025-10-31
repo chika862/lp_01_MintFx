1c48af7468cfc2ad09ed4cd98bab4713363b5113
pin863 <chika67zz@gmail.com>
2025-10-31 15:59:46 +0900

    about直った

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.js"],
+  content: ["./*.html", "./about/**/*.html", "./src/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
